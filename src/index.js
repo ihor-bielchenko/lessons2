96dd9f51273e5c3b5cda09662fce4b22bbc556be
ihor <ihor.bielchenko@drivedatum.com>
2021-11-13 12:00:24 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-var hello;
 
-hello = [];
+let var1 = 1;
 
-// var text = 'hello';
+console.log('var1', var1);
 
-console.log('hello', typeof hello);
+if (true) {
+	let var1 = 2;
 
-// prompt('хууй');
+	console.log('var1', var1);
+}
+
+console.log('var1', var1);
+
+// console.log('obj1', arr1.length);
